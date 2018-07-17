bc69d0fd8d3a0c14655a31f716be99a821da1be2
Ben Struhl <struhl.b@husky.neu.edu>
2018-07-16 23:48:22 -0400

    added more boilerplate

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/** 
+ * Prints a file silently to a given printer
+ * @param {string} file
+ * @param {string} printer
+ */
+
+module.exports = function print(file, printer) {
+    
+}
