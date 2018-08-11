715ddc6abab11daf08032faf8405f0218d54cd7b
Ben Struhl <struhl.b@husky.neu.edu>
2018-08-10 20:13:09 -0400

    made tests more robust and up to date

--- test/funcs.spec.js
+++ test/funcs.spec.js
@@ -1,16 +1,24 @@
-import { detectOS, printFile } from "../src/funcs"
+import { detectOS, printFile } from '../src/funcs'
 
 const expect = require('chai').expect;
+const assert = require('chai').assert;
 
 describe('funcs', function() {
     it('should have detected the right OS of the platform', function() {
         expect(detectOS()).to.satisfy(function(str) { 
-            return str === 'unix' ||
-            str === 'windows';
+            if(process.platform === 'linux' || process.platform === 'darwin') {
+                return str === 'unix';
+            }  else if(process.platform  === 'win32') {
+                return str === 'windows';
+            } else {
+                throw new Error('Unsupported OS Error: Use MacOS, Windows, or Linux');
+            }
         });
     });
-    it('should not error out on printing a document that exists to a specific printer', function() {
-        let isWindows = detectOS() === 'windows' ? true :  false;
-        expect(printFile('..\\SecretPrint\\resources\\crab.png', 'test', isWindows))
-    })
+    it('should throw an error for an unsupported OS', function() {
+        if(process.platform != 'linux' && process.platform != 'darwin' && process.platform != 'win32') {
+          assert.throw(detectOS, 'Unsupported OS Error: Use MacOS, Windows, or Linux');
+        }
+    });
+    it('should not error out on printing a document that exists to a specific printer');
 });
--- test/secretPrint.spec.js
+++ test/secretPrint.spec.js
@@ -2,5 +2,6 @@ var expect = require('chai').expect;
 var SecretPrint = require('../src/index');
 
 describe('secretPrint', function() {
-    it('should not error out on print');
+    it('should not error out when printing a file to a default printer');
+    it('should not error out when printing a file to a specific printer');
 });
