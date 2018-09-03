84a92a6da7e70a437f3a76e39a8f6d6217ed6d3d
Ben Struhl <struhl.b@husky.neu.edu>
2018-09-03 13:58:22 -0400

    added ipp printer back

--- src/printer.js
+++ src/printer.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const Printer = require('ipp-printer')
 
-const printer = new Printer('My Printer')
+const printer = new Printer('StruhlsTestPrinter')
 
 printer.on('job', function (job) {
   console.log('[job %d] Printing document: %s', job.id, job.name)
